3c2aa51d652050f756a2078cd6e46c4f69b45ae5
Kristian <10221425@ucn.dk>
2024-07-18 07:11:01 +0200

    Logout done

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,10 +1,23 @@
-import  { useState } from "react";
 import { UserAuth } from "../context/AuthContext";
+import { logout } from "../functions/auth";
 
 export default function UserDashboard() {
   const { user } = UserAuth();
 
+  const handleLogout = () => {
+    logout();
+  };
+
   return (
-    <div>Welcome {user?.firstName} </div>
-  )
+    <>
+      <div>Welcome {user?.firstName}</div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline block mt-12"
+        onClick={handleLogout}
+      >
+        Logout
+      </button>
+    </>
+  );
 }
