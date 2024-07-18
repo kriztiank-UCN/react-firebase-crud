0d2df5ec688fc786c1f9675edb6be8a6b0394ab8
Kristian <10221425@ucn.dk>
2024-07-18 05:40:23 +0200

    NavBar created, only visible to NonUserRoutes

--- src/routes/NonUserRoutes.jsx
+++ src/routes/NonUserRoutes.jsx
@@ -2,19 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import CreateUser from "../pages/CreateUser";
 import Login from "../pages/Login";
-// import NavBar from "../components/NavBar";
+import NavBar from "../components/NavBar";
 
 export default function NonUserRoutes() {
   return (
     <div>
-      {/* <NavBar> */}
+      <NavBar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-user" element={<CreateUser />} />
           <Route path="/login" element={<Login />} />
           <Route path="/*" element={<Home />} />
         </Routes>
-      {/* </NavBar> */}
+      </NavBar>
     </div>
   );
 }
