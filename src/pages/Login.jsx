e85a7a85a3d88e4310e1203becbdc1cacc8a2591
Kristian <10221425@ucn.dk>
2024-07-18 07:30:03 +0200

    Login done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,37 @@
+import { useState } from "react"
+import { signIn } from "../functions/auth"
+
 export default function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = () => {
+    // pass parameters from the signIn function to the argument of the signIn
+    signIn(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={e => {
+          setEmail(e.target.value)
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={e => {
+          setPassword(e.target.value)
+        }}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+    </div>
   )
 }
